9e8ba0c293c5ae37d40cde19467ecbd4bc5ab85e
Nicolò Taormina <59546514+CodeClimberNT@users.noreply.github.com>
2024-12-24 15:10:06 +0100

    Added odd solutions

--- solution.py
+++ solution.py
@@ -10,14 +10,15 @@ def f2(x: np.ndarray) -> np.ndarray:
 
 
 def f3(x: np.ndarray) -> np.ndarray:
-    raise NotImplementedError("Equation not yet implemented")
+    return (np.arctan2(-6.732, -3.574)) * ((np.sinh(x[1]) - np.cosh(3.019)) + x[2])
 
 
 def f4(x: np.ndarray) -> np.ndarray:
     return np.sinh((np.log(3.237) * (np.cos(x[0]) - (x[0] / x[0])))) + np.exp(np.abs(np.cos(x[0]) - np.reciprocal(-0.727)))
 
+
 def f5(x: np.ndarray) -> np.ndarray:
-    raise NotImplementedError("Equation not yet implemented")
+    return np.log(np.sign(x))
 
 
 def f6(x: np.ndarray) -> np.ndarray:
@@ -25,7 +26,11 @@ def f6(x: np.ndarray) -> np.ndarray:
 
 
 def f7(x: np.ndarray) -> np.ndarray:
-    raise NotImplementedError("Equation not yet implemented")
+    return 1 / np.tan(
+        np.arctanh(
+            (np.sin(9.050) ** np.cosh(np.sinh((np.arctan2(x[1], np.abs(x[1] - x[0]))))))
+        )
+    )
 
 
 def f8(x: np.ndarray) -> np.ndarray:
