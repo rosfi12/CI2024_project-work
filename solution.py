49452e4f7204a1960b4f920be9501bd5ce3f6809
Nicolò Taormina <nicktaormina3@gmail.com>
2024-12-24 15:08:07 +0100

    added solution

--- solution.py
+++ solution.py
@@ -10,7 +10,7 @@ def f2(x: np.ndarray) -> np.ndarray:
 
 
 def f3(x: np.ndarray) -> np.ndarray:
-    raise NotImplementedError("Equation not yet implemented")
+    return (np.arctan2(-6.732, -3.574)) * ((np.sinh(x[1]) - np.cosh(3.019)) + x[2])
 
 
 def f4(x: np.ndarray) -> np.ndarray:
@@ -18,7 +18,7 @@ def f4(x: np.ndarray) -> np.ndarray:
 
 
 def f5(x: np.ndarray) -> np.ndarray:
-    raise NotImplementedError("Equation not yet implemented")
+    return np.log(np.sign(x))
 
 
 def f6(x: np.ndarray) -> np.ndarray:
@@ -26,7 +26,11 @@ def f6(x: np.ndarray) -> np.ndarray:
 
 
 def f7(x: np.ndarray) -> np.ndarray:
-    raise NotImplementedError("Equation not yet implemented")
+    return 1 / np.tan(
+        np.arctanh(
+            (np.sin(9.050) ** np.cosh(np.sinh((np.arctan2(x[1], np.abs(x[1] - x[0]))))))
+        )
+    )
 
 
 def f8(x: np.ndarray) -> np.ndarray:
